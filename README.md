acaca33375150b923d6f5ee211ad0722f915cc83
unknown <lxe@lxe.co>
2023-03-14 22:37:41 -0700

    devel

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This is my first go at ML tuning, so this is probably very wrong. This should work on a single ~~3090 GPU~~ A100 and takes 3 hours to train 250 setps on a subset of 1000 samples. Full 50k~ dataset should take ~19 hours. There's a lot of knobs to turn that I don't understand yet.
 
+Wishlist:
+
+- [ ] Command Flags (Currently everything is in code)
+- [ ] Auto Tuning 
+- [X] Windows Support (Even with DeepSpeed!)
+
 **I'm currently running this, so I don't know if it even works**
 
 References:
@@ -9,19 +15,53 @@ References:
  - https://github.com/facebookresearch/llama/issues/169
  - https://github.com/huggingface/transformers/pull/21955 
 
-Prereqs
+### Prereqs
+
+Installing pytorch and cuda is the hardest part of machine learning
+I've come up with this install line from the following sources:
+
+- https://pytorch.org/get-started/locally/#start-locally
+- https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/index.html#installing-previous-cuda-releases
+
 ```
-conda install -y cuda pytorch-cuda=11.7 -c pytorch -c nvidia
+conda install -y cuda pybind pytorch=1.13.1 torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia/label/cuda-11.7.0
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+python -c 'import torch; print(torch.cuda.is_available())'
 ```
 
-Run tuning
+For Windows, you'll need to compile DeepSpeed (which actually works!)
+
+Install [Visual Studio 2019 Build Tools](https://learn.microsoft.com/en-us/visualstudio/releases/2019/history#release-dates-and-build-numbers). Click on the latest **BuildTools** link, Select **Desktop Environment with C++** when installing
+
+In an **Administrator Powershell**, do:
+
+```
+git clone https://github.com/microsoft/DeepSpeed.git repositories/DeepSpeed
+cd repositories/DeepSpeed
+.\build_win.bat
+pip install (Get-Item .\dist\*)
+```
+
+Get data, model, and requirements:
+
 ```
 git clone https://github.com/tatsu-lab/stanford_alpaca repositories/stanford_alpaca
 pip install -r requirements.txt
 python download-model.py decapoda-research/llama-7b-hf
+```
+
+Run tuning:
+
+```
+export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig
 deepspeed tune.py
 ```
 
+Or, for Windows Powershell:
+```
+$env:CUDA_VISIBLE_DEVICES=0
+python (Get-Command ds).Path tune.py
+``` 
 
 Comparing the model tuned on 250-step 1000 training samples VS vanilla llama-7b:
 
--- tune.py
+++ tune.py
@@ -1,4 +1,8 @@
+import sys
+import os
 import torch
+from torch import distributed
+import deepspeed
 from transformers import LLaMATokenizer, LLaMAForCausalLM, LLaMAConfig, TrainingArguments, Trainer, DataCollatorForLanguageModeling
 from datasets import Dataset
 import json
@@ -6,6 +10,60 @@ import json
 # It's not pretty down there, sorry
 
 MODEL_PATH = 'models/llama-7b-hf'
+
+# https://github.com/Tencent/TencentPretrain/blob/33dbf6635eabf9efa92dacd1bad6a2d03143fa47/models/deepspeed_config.json#L4
+ds_config_dict = {
+    "amp": {
+        "enabled": True,
+    },
+    "accelerator": "cpu",
+    "gradient_accumulation_steps": 1,
+    "train_micro_batch_size_per_gpu": 1,
+    "steps_per_print": 100,
+    "optimizer": {
+        "type": "Adam",
+        "params": {
+            # "lr": 1e-4,
+            # "weight_decay": 1e-2
+            # Match what's in https://github.com/tatsu-lab/stanford_alpaca#fine-tuning
+            "lr": 2e-5,
+            "weight_decay": 1,
+            "comm_backend_name": "nccl" if sys.platform != "win32" else "gloo"
+        }
+    },
+    "flops_profiler": {
+        "enabled": False,
+        "profile_step": 1,
+        "module_depth": -1,
+        "top_modules": 3,
+        "detailed": True
+    },
+    "fp16": {
+        "enabled": True,
+        "loss_scale": 0,
+        "loss_scale_window": 1000,
+        "hysteresis": 2,
+        "min_loss_scale": 1
+    },
+    "zero_optimization": {
+        "enabled": False,
+        "stage": 1,
+        "cpu_offload": True,
+        "contiguous_gradients": True,
+        "overlap_comm": True,
+        "reduce_scatter": False,
+        "reduce_bucket_size": 5e7,
+        "allgather_bucket_size": 5e7
+    },
+    "activation_checkpointing": {
+        "partition_activations": False,
+        "contiguous_memory_optimization": False,
+        "cpu_checkpointing": False
+    },
+    "wall_clock_breakdown": False,
+    "zero_allow_untested_optimizer": True,
+}
+
 
 def get_llama(model):
     def skip(*args, **kwargs):
@@ -18,24 +76,10 @@ def get_llama(model):
     model.seqlen = 2048
     return model
 
-model = get_llama(MODEL_PATH)
-model.eval()
-model.to(torch.device('cuda:0'))
-
-config = LLaMAConfig()
-
-tokenizer = LLaMATokenizer.from_pretrained(
-    MODEL_PATH, 
-    max_length=512, 
-    truncation=True, 
-    return_overflowing_tokens=True, 
-    config=config
-)
-
-tokenizer.pad_token = config.pad_token_id
 
 def tokenize_function(allEntries):
     return tokenizer(allEntries['text'], padding='max_length', truncation=True, max_length=512)
+
 
 def to_texts(entry):
     # From https://github.com/tatsu-lab/stanford_alpaca#data-release
@@ -50,106 +94,89 @@ def to_texts(entry):
 ### Response:
 {entry['output']}"""
 
-with open('repositories/stanford_alpaca/alpaca_data.json', 'r') as f:
-    data = json.load(f)
 
-texts = list(map(to_texts, data))
-dataset = Dataset.from_dict({"text": texts})
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
+if __name__ == '__main__':
+    if sys.platform == "win32":
+        # torch._C._c10d_init = None
+        # with torch.no_grad():
+        distributed.init_process_group(backend='gloo')
 
-# Split the tokenized dataset into training and evaluation datasets
-split = tokenized_dataset.train_test_split(test_size=0.2)
-train_tokenized_dataset = split['train']
-eval_tokenized_dataset = split['test']
+    model = get_llama(MODEL_PATH)
+    # model.eval()
+    # model.to(torch.device('cuda:0'))
 
-# Print the sizes of the datasets
-print("Full Train dataset size:", len(train_tokenized_dataset))
-print("Full Eval dataset size:", len(eval_tokenized_dataset))
+    config = LLaMAConfig()
 
-# Make small datasets for testing
-small_train_dataset = train_tokenized_dataset.shuffle(seed=42).select(range(1000))
-small_eval_dataset = eval_tokenized_dataset.shuffle(seed=46).select(range(1000))
+    tokenizer = LLaMATokenizer.from_pretrained(
+        MODEL_PATH,
+        max_length=512,
+        truncation=True,
+        return_overflowing_tokens=True,
+        config=config
+    )
 
-print("Small Train dataset size:", len(small_train_dataset))
-print("Small Eval dataset size:", len(small_eval_dataset))
+    tokenizer.pad_token = config.pad_token_id
 
-# https://github.com/Tencent/TencentPretrain/blob/33dbf6635eabf9efa92dacd1bad6a2d03143fa47/models/deepspeed_config.json#L4
-ds_config_dict = {
-  "gradient_accumulation_steps": 'auto',
-  "train_micro_batch_size_per_gpu": 'auto',
-  "steps_per_print": 100,
-  "optimizer": {
-    "type": "Adam",
-    "params": {
-        # "lr": 1e-4,
-        # "weight_decay": 1e-2
-        # Match what's in https://github.com/tatsu-lab/stanford_alpaca#fine-tuning
-        "lr": 2e-5,
-        "weight_decay": 1
-    }
-  },
-  "flops_profiler": {
-    "enabled": False,
-    "profile_step": 1,
-    "module_depth": -1,
-    "top_modules": 3,
-    "detailed": True
-  },
-  "fp16": {
-    "enabled": True,
-    "loss_scale": 0,
-    "loss_scale_window": 1000,
-    "hysteresis": 2,
-    "min_loss_scale": 1
-  },
-  "zero_optimization": {
-    "stage": 1,
-    "cpu_offload": True,
-    "contiguous_gradients": True,
-    "overlap_comm": True,
-    "reduce_scatter": False,
-    "reduce_bucket_size": 5e7,
-    "allgather_bucket_size": 5e7
-  },
-  "activation_checkpointing": {
-    "partition_activations": False,
-    "contiguous_memory_optimization": False,
-    "cpu_checkpointing": False
-  },
-  "wall_clock_breakdown": False,
-  "zero_allow_untested_optimizer": True,
-}
+    with open('repositories/stanford_alpaca/alpaca_data.json', 'r') as f:
+        data = json.load(f)
 
-# https://github.com/tatsu-lab/stanford_alpaca#fine-tuning
-training_args = TrainingArguments(
-    output_dir="output", 
-    learning_rate=2e-5,
-    num_train_epochs=1, # 3
-    weight_decay=1,
-    do_eval=True,
-    evaluation_strategy="epoch",
-    fp16=True,
-    deepspeed=ds_config_dict,
-    save_strategy="steps",
-    save_steps=250, # 500
-)
+    texts = list(map(to_texts, data))
+    dataset = Dataset.from_dict({"text": texts})
+    tokenized_dataset = dataset.map(tokenize_function, batched=True)
 
-data_collator = DataCollatorForLanguageModeling(
-    tokenizer=tokenizer, mlm=False
-)
+    # Split the tokenized dataset into training and evaluation datasets
+    split = tokenized_dataset.train_test_split(test_size=0.2)
+    train_tokenized_dataset = split['train']
+    eval_tokenized_dataset = split['test']
 
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+    # Print the sizes of the datasets
+    print("Full Train dataset size:", len(train_tokenized_dataset))
+    print("Full Eval dataset size:", len(eval_tokenized_dataset))
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=data_collator,
-    train_dataset=small_train_dataset, # Change to train_tokenized_dataset for full training
-    eval_dataset=small_eval_dataset,   # Change to eval_tokenized_dataset for full training
-    compute_metrics=compute_metrics,
-)
+    # Make small datasets for testing
+    small_train_dataset = train_tokenized_dataset.shuffle(
+        seed=42).select(range(1000))
+    small_eval_dataset = eval_tokenized_dataset.shuffle(
+        seed=46).select(range(1000))
 
-trainer.train()
+    print("Small Train dataset size:", len(small_train_dataset))
+    print("Small Eval dataset size:", len(small_eval_dataset))
+
+    # https://github.com/tatsu-lab/stanford_alpaca#fine-tuning
+    training_args = TrainingArguments(
+        per_device_train_batch_size=1,
+        output_dir="output",
+        learning_rate=2e-5,
+        num_train_epochs=1,  # 3
+        weight_decay=1,
+        do_eval=True,
+        evaluation_strategy="epoch",
+        fp16=False,
+        deepspeed=ds_config_dict,
+        save_strategy="steps",
+        save_steps=250,  # 500,
+    )
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=False
+    )
+
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    trainer = Trainer(
+        deepspeed=ds_config_dict,
+        model=model,
+        # args=training_args,
+        data_collator=data_collator,
+        # Change to train_tokenized_dataset for full training
+        train_dataset=small_train_dataset,
+        # Change to eval_tokenized_dataset for full training
+        eval_dataset=small_eval_dataset,
+        compute_metrics=compute_metrics,
+    )
+
+    with torch.no_grad():
+        trainer.train()
